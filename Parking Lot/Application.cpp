5c0686f50cdd3a2c7c679a3ee89e352141bf17aa
shaktimaan0572 <vivekmajumdar03@gmail.com>
2018-10-01 20:43:28 +0530

    new changes

--- Parking Lot/Application.cpp
+++ Parking Lot/Application.cpp
@@ -1,4 +1,36 @@
-
+#include<iostream>
+using namespace std;
+#include"stdlib.h"
+#include "ParkingLot.h"
 void run(){
+    ParkingLot p;
+    //system("cls");
+    p.ParkingLotSetup();
+    int w;
+    bool f1=true;
+    while(f1){
+        cin.get();
+        system("cls");
+        cout<<"\tEnter 1. For Vehicle Entry"<<endl;
+        cout<<"\tEnter 2. For Vehicle Exit"<<endl;
+        cout<<"\tEnter 3. To see the Parking Lot Status"<<endl;
+        cout<<"\tEnter 4. To Exit"<<endl<<endl;
+        cin>>w;
+        switch(w)
+        {
+            case 1: p.VehicleEntry();
+            break;
+            case 2: p.VehicleExit();
+            break;
+            case 3: p.Show();
+            break;
+            case 4: f1=false;
+            break;
+            default:
+                cout<<"Enter valid option"<<endl;
+            break;
+        }
+    }
+    p.deleteBlock();
 
 }
--- Parking Lot/Vehicle.cpp
+++ Parking Lot/Vehicle.cpp
@@ -4,18 +4,18 @@ using namespace std;
 Vehicle::Vehicle()
 {
     //ctor
-    cout<<"vehicle created"<<endl;
+    //cout<<"vehicle created"<<endl;
 
 }
 
 Vehicle::~Vehicle()
 {
     //dtor
-    cout<<"vehicle destroyed"<<endl;
+    //cout<<"vehicle destroyed"<<endl;
 }
 
 Vehicle::Vehicle(const Vehicle& other)
 {
     //copy ctor
-    cout<<"vehicle copy created"<<endl;
+    //cout<<"vehicle copy created"<<endl;
 }
